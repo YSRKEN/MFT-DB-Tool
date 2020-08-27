98e7fca7198176bbf6b346f1a775432076896763
YSRKEN <ysr.ken@gmail.com>
2020-08-27 20:24:46 +0900

    [add] レンズデータのJSONを組み込み、それがReact＋TypeScriptで読み込めることを確認した

--- mft-db-tool/src/App.tsx
+++ mft-db-tool/src/App.tsx
@@ -1,26 +1,56 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import 'App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+interface Lens {
+  filter_diameter: number
+  has_image_stabilization: boolean
+  id: number
+  is_drip_proof: boolean
+  is_inner_zoom: boolean
+  maker: string
+  max_photographing_magnification: number
+  name: string
+  overall_diameter: number
+  overall_length: number
+  price: number
+  product_number: string
+  telephoto_f_number: number
+  telephoto_focal_length: number
+  telephoto_min_focus_distance: number
+  weight: number
+  wide_f_number: number
+  wide_focal_length: number
+  wide_min_focus_distance: number
+}
+
+const App: React.FC = () => {
+  const [lensList, setLensList] = useState<Lens[]>([]);
+
+  fetch('./lens_data.json').then(res => {
+    if (res.ok) {
+      res.json().then(data => {
+        setLensList(data);
+      });
+    }
+  });
+
+  return (<>
+    <h1>レンズデータベース</h1>
+    <table cellSpacing={0} cellPadding={5}>
+      <thead>
+        <th>メーカー</th>
+        <th>レンズ名</th>
+        <th>価格(税抜)</th>
+      </thead>
+      <tbody>
+        {lensList.map(lens => <tr>
+          <td>{lens.maker}</td>
+          <td>{lens.name}</td>
+          <td>{lens.price}</td>
+        </tr>)}
+      </tbody>
+    </table>
+  </>);
 }
 
 export default App;
