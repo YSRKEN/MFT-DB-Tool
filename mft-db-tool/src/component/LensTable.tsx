f2d0ab7f579b1531d50dc50b4a21769dc26b41fa
YSRKEN <ysr.ken@gmail.com>
2020-09-16 19:11:12 +0900

    [add] レンズマウントについての情報を表示できるようにした。また、フィルター径について、装着不可ならその旨を表示するようにした

--- mft-db-tool/src/component/LensTable.tsx
+++ mft-db-tool/src/component/LensTable.tsx
@@ -61,9 +61,14 @@ const LensTable: React.FC<{ lensList: Lens[] }> = ({ lensList }) => {
                   ? `${lensListA[lensIndex].wide_min_focus_distance / 1000}m`
                   : `${lensListA[lensIndex].wide_min_focus_distance / 1000}m～${lensListA[lensIndex].telephoto_min_focus_distance / 1000}m`}</li>
                 <li>換算最大撮影倍率：{lensListA[lensIndex].max_photographing_magnification}倍</li>
-                <li>フィルター径：{lensListA[lensIndex].filter_diameter}mm</li>
+                <li>フィルター径：{
+                  lensListA[lensIndex].filter_diameter >= 1
+                  ? `${lensListA[lensIndex].filter_diameter}mm`
+                  : '装着不可'
+                }</li>
                 <li>最大径×全長：{lensListA[lensIndex].overall_diameter}mm×{lensListA[lensIndex].overall_length}mm</li>
                 <li>質量：{lensListA[lensIndex].weight}g</li>
+                <li>レンズマウント：{lensListA[lensIndex].mount}</li>
                 <li>その他属性：
                   {lensListA[lensIndex].is_drip_proof ? '防塵防滴　' : ''}
                   {lensListA[lensIndex].has_image_stabilization ? 'レンズ内手ブレ補正　' : ''}
