86e4d0b29b12bbc7e87c8c87840f7b1c2af3fac1
YSRKEN <ysr.ken@gmail.com>
2021-05-03 13:44:50 +0900

    [add] SAMYANG用のコードの土台を作成した

---
+++ server/service/maker/samyang.py
@@ -0,0 +1,7 @@
+from pandas import DataFrame
+
+from service.i_scraping_service import IScrapingService
+
+
+def get_samyang_lens_list(scraping: IScrapingService) -> DataFrame:
+    return DataFrame()
